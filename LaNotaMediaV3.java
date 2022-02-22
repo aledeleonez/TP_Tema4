74f946c4e229d99562e47d9e1e0323bb20bbf9cc
aledeleonez <98777405+aledeleonez@users.noreply.github.com>
2022-02-22 12:33:48 +0100

    Ejercicio 3 terminado

---
+++ LaNotaMediaV3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LaNotaMediaV3 {
+    
+    public void notaMedia(Scanner teclado){
+        double nota = 0;
+        double suma = 0;
+        int contador = 0;
+        int contador2 = 0;
+        double media = 0;
+        double porcentaje;
+        while(nota != -1){
+            System.out.println("¿Nota (-1 para terminar)?: ");
+            nota = teclado.nextDouble();
+            suma += nota;
+            contador++;
+            if(nota >= 10){
+                contador2 ++;
+            }
+        }
+        media = suma/contador;
+        porcentaje = contador2*100/contador;
+        System.out.println(String.format("La media es %.2f (%.2f %% >= 10)", media, porcentaje));
+    }
+}
