1b0815946c55d2e5293b73c9896831b014ce6223
Pedro Manuel <98824525+PedroLopezG@users.noreply.github.com>
2022-03-01 22:05:57 +0100

    actualizado, no terminado

--- App.java
+++ App.java
@@ -1,11 +1,87 @@
+package tema4;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
 
-        Scanner teclado = new Scanner(System.in);
 
-        LaNotaMediaV2 media = new LaNotaMediaV2();
-        media.notaMedia(teclado);
+
+        Scanner teclado = new Scanner(System.in);
+        int eleccion = 0;
+
+        while (eleccion != 12) {
+            menu();
+            eleccion = teclado.nextInt();
+
+            switch (eleccion) {
+
+                case 1:
+                    LaNotaMediaV1 ejercicio1 = new LaNotaMediaV1();
+                    ejercicio1.notaMedia(teclado);
+                    break;
+                case 2:
+                    LaNotaMediaV2 ejercicio2 = new LaNotaMediaV2();
+                    ejercicio2.notaMedia(teclado);
+                    break;
+                case 3:
+                    LaNotaMediaV3 ejercicio3 = new LaNotaMediaV3();
+                    ejercicio3.notaMedia(teclado);
+                    break;
+                case 4:
+                    AdivinarNumero ejercicio4 = new AdivinarNumero();
+                    ejercicio4.adivinarNum(teclado);
+                    break;
+                case 5:
+                    EncontrarloYo ejercicio5 = new EncontrarloYo();
+                    ejercicio5.numero(teclado);
+                    break;
+                case 6:
+                    CapitalFrancia ejercicio6 = new CapitalFrancia();
+                    ejercicio6.capital(teclado);
+                    break;
+                case 7:
+
+                    break;
+                case 8:
+
+                    break;
+                case 9:
+
+                    break;
+                case 10:
+                    ArteASCIIv1 ejercicio10 = new ArteASCIIv1();
+                    ejercicio10.figura(teclado);
+                    break;
+                case 11:
+
+                    break;
+
+
+
+            }
+        }
+        System.out.println("Saliendo . . .");
     }
+
+
+    private static void menu(){
+        System.out.println("\n\nQue ejercicio quieres ver: \n" );
+        System.out.println("1. La nota media (versión 1)");
+        System.out.println("2. La nota media (versión 2)");
+        System.out.println("3. La nota media (versión 3)");
+        System.out.println("4. Adivine en qué número estoy pensando");
+        System.out.println("5. Ahora tengo que encontrarlo yo");
+        System.out.println("6. ¿Qué hace este algoritmo?");
+        System.out.println("7. Visualización de citas de películas (versión 1)");
+        System.out.println("8. Visualización de citas de películas (versión 2)");
+        System.out.println("9. Escribir un múltiplo de tres");
+        System.out.println("10. Arte ASCII 1");
+        System.out.println("11. Arte ASCII 2");
+        System.out.println("12. Salir");
+    }
+
+
+
+
+
 }
